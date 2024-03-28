13e73af6ecd8a6ae68ac840b408b6b1db5636589
Andrei Tarasevich <eansable@yandex.by>
2024-03-28 16:08:39 +0300

    create base and add some tables

--- next_js_app/app/account/store/api.ts
+++ next_js_app/app/account/store/api.ts
@@ -2,6 +2,7 @@ import { sql } from "@vercel/postgres";
 
 export const fetchRegistration = async () => {
     try {
+        
 
     } catch (err) {
         console.error("Error when registration:", err);
